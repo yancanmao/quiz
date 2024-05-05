68fa15d4a9ed8787961ef249ab51673293ab2e07
Your Name <you@example.com>
2024-05-05 22:37:15 +0800

    first commit

---
+++ stateful/app-stateful.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Try reading from ConfigMap first
+        if os.path.exists('/data/hello.txt'):
+            with open('/data/hello.txt', 'r') as file:
+                message = file.read()
+        else:
+            message = "File/Config Not Found"
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler)
+    httpd.serve_forever()
+
